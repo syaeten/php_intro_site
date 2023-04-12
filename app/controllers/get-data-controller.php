f8dbb639e6024a51946ba8be86b68c2b8f5ea81e
syaeten <sya.eten@canbright.co.jp>
2023-04-12 12:24:42 +0900

    some css

--- app/controllers/get-data-controller.php
+++ app/controllers/get-data-controller.php
@@ -3,24 +3,9 @@ require_once '../models/Article.php';
 require_once '../../config/config.php';
 
 function get_articles(){
-  global $pdo;//
-  /*$host = 'localhost';
-  $dbname = 'php_intro_site_db';
-  $user = 'root';
-  $password = '1234';
 
-  // 创建PDO连接  Create PDO connection
-  $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-  $options = [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES => false,
-  ];
-  try {
-      $pdo = new PDO($dsn, $user, $password, $options);
-  } catch (PDOException $e) {
-      throw new PDOException($e->getMessage(), (int)$e->getCode());
-  }*/
+  global $pdo;  //global访问全局变量 Access global variables.
+
 
   // 执行查询语句  Execute query statement
   $sql = "SELECT * FROM article";
--- public/partials/nav.php
+++ public/partials/nav.php
@@ -5,10 +5,12 @@
   <nav>
     <ul>
       <!-- * 路径别忘了最前面也要写'/', 不然连续跳时路径会叠加起来! -->
+      <!-- * Don't forget to include '/' at the beginning of the path ! -->
       <li><a href="/app/views/php_basic.php">PHP基础语法</a></li>
       <li><a href="/app/views/php_oop.php">PHP类和对象</a></li>
       <li><a href="/app/views/mysql_basic.php">MySQL</a></li>
       <li><a href="/app/views/jquery_basic.php">jQuery</a></li>
+      <li><a href="/app/views/add_new_article.php" id='add_new_btn'>Add New</a></li>
     </ul>
   </nav>
 </header>
