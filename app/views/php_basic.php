ba59c4cf79b03c5635d2fd5c523b8e3d81dfecca
syaeten <sya.eten@canbright.co.jp>
2023-04-27 11:42:53 +0800

    xx

--- app/views/php_basic.php
+++ app/views/php_basic.php
@@ -1,25 +1,26 @@
 <?php
-  require_once '../../public/partials/header.php';
-  require_once '../../public/partials/nav.php';
+require_once '../../public/partials/header.php';
+require_once '../../public/partials/nav.php';
 ?>
 <?php
-$articles=get_articles();
+$articles = get_articles();
 
 ?>
+
 <?php
-foreach ($articles as $article):
+foreach ($articles as $article) :
 ?>
 
-<div>
-  <h2>PHP 基础语法 (Basic syntax of PHP)</h2>
-  <h2><?=$article['title']?></h2>
-  <p><?=$article['content']?></p>
-</div>
+  <div>
+    <h2>PHP 基础语法 (Basic syntax of PHP)</h2>
+    <h2><?= $article['title'] ?></h2>
+    <p><?= $article['content'] ?></p>
+  </div>
 
 <?php
 endforeach;
 ?>
 
 <?php
-  require_once '../../public/partials/footer.php';
+require_once '../../public/partials/footer.php';
 ?>
